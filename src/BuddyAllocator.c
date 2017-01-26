05f85bf2bd86888e027d0392cbff441cba90bf49
kshk123 <kshk123@gmail.com>
2017-01-26 21:27:27 +0530

    Further enhancements

--- src/BuddyAllocator.c
+++ src/BuddyAllocator.c
@@ -1,180 +1,187 @@
+#include <string.h>
+#include <stdio.h>
 #include "BuddyAllocator.h"
-#include "string.h"
-#include "stdio.h"
 
-int main()
+buddy_allocator_t *buddy_allocator_create(size_t raw_memory_size)
 {
-	int size = 500;
-	void * mem = (void *)malloc(size);
+    buddy_allocator_t * newBuddy = (buddy_allocator_t *)malloc(sizeof(buddy_allocator_t));
+    newBuddy->memStart = malloc(raw_memory_size);
+    if(NULL == newBuddy->memStart)
+    {
+        printf("Failed to allocate memory.\n");
+        return NULL;
+    }
+    newBuddy->memSize  = raw_memory_size;
+    if(!initBuddyAlloc(newBuddy))
+    {
+        free(newBuddy->memStart);
+        free(newBuddy);
+        return NULL;
+    }
 
-	buddy_allocator_t * buddyT = buddy_allocator_create(mem, size);
-	void * ptr = buddy_allocator_alloc(buddyT, 30);
-	memcpy(ptr, "abcdef", 6);
-	printf("%s\n", (char *)ptr);
-	buddy_allocator_free(buddyT, ptr);
-	buddy_allocator_destroy(buddyT);
+    node * newNode = (node *)malloc(sizeof(node));
+    newNode->buddy_alloc = newBuddy;
+    newNode->next = NULL;
+    
+    if(NULL == buddyList)
+        buddyList = newNode;
+    else
+    {
+        node * ptr = buddyList;
+        while(ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = newNode;
+    }
 
-	return 0;
-}
-
-buddy_allocator_t *buddy_allocator_create(void *raw_memory, size_t raw_memory_size)
-{
-	buddy_allocator_t * newBuddy = (buddy_allocator_t *)malloc(sizeof(buddy_allocator_t));
-	newBuddy->memStart = raw_memory;
-	newBuddy->memSize  = raw_memory_size;
-	initBuddyAlloc(newBuddy);
-
-	node * newNode = (node *)malloc(sizeof(node));
-	newNode->buddy_alloc = newBuddy;
-	newNode->next = NULL;
-	
-	if(NULL == buddyList)
-		buddyList = newNode;
-	else
-	{
-		node * ptr = buddyList;
-		while(ptr->next != NULL)
-		{
-			ptr = ptr->next;
-		}
-		ptr->next = newNode;
-	}
-
-	return newBuddy;
+    return newBuddy;
 }
 
 void buddy_allocator_destroy(buddy_allocator_t *buddy_allocator)
 {
-	node * ptr = buddyList;
-	node * prev = NULL;
+    node * ptr = buddyList;
+    node * prev = NULL;
 
-	while(ptr != NULL && ptr->buddy_alloc != buddy_allocator)
-	{
-		prev = ptr;
-		ptr = ptr->next;
-	}
+    while(ptr != NULL && ptr->buddy_alloc != buddy_allocator)
+    {
+        prev = ptr;
+        ptr = ptr->next;
+    }
 
-	if(ptr != NULL)
-	{
-		if(NULL == prev) /* deleting at the head */
-		{
-			buddyList = ptr->next;
-		}
-		else
-		{
-			prev->next = ptr->next;
-		}
-		free(buddy_allocator);
-		buddy_allocator = NULL;
-		free(ptr);
-	}
+    if(ptr != NULL)
+    {
+        if(NULL == prev) /* deleting at the head */
+        {
+            buddyList = ptr->next;
+        }
+        else
+        {
+            prev->next = ptr->next;
+        }
+        free(buddy_allocator->memStart);
+        buddy_allocator->memStart = NULL;
+        free(buddy_allocator);
+        buddy_allocator = NULL;
+        free(ptr);
+        ptr = NULL;
+    }
 }
 
-void initBuddyAlloc(buddy_allocator_t *buddy_allocator)
+int initBuddyAlloc(buddy_allocator_t *buddy_allocator)
 {
-	uint8_t order = 0;
-	while(ORDERTOSIZE(order) < buddy_allocator->memSize)
-	{
-		order++;
-	}
+    uint8_t order = 0;
+    while(ORDERTOSIZE(order) < buddy_allocator->memSize)
+    {
+        order++;
+    }
 
-	buddy_allocator->maxOrder = order;
-	memset(buddy_allocator->memPool, 0, MAX_ORDER);
-	buddy_allocator->memPool[order] = buddy_allocator->memStart;
+    if(order > MAX_ORDER)
+    {
+        printf("Cannot create buddy allocator as backup memory is more than can be supported\n");
+        return FAIL;
+    }
+
+    buddy_allocator->maxOrder = order;
+    memset(buddy_allocator->memPool, 0, MAX_ORDER + 1);
+    buddy_allocator->memPool[order] = buddy_allocator->memStart;
+    return SUCCESS;
 }
 
-void * buddy_allocator_alloc(buddy_allocator_t *buddy_allocator, size_t size)
+void * buddy_allocator_alloc(buddy_allocator_t *buddy_allocator, size_t size, uint8_t * order)
 {
-	node * ptr = buddyList;
-	while(ptr != NULL && ptr->buddy_alloc != buddy_allocator)
-	{
-		ptr = ptr->next;
-	}
+    node * ptr = buddyList;
+    while(ptr != NULL && ptr->buddy_alloc != buddy_allocator)
+    {
+        ptr = ptr->next;
+    }
 
-	if(NULL == ptr)
-		return NULL;
+    if(NULL == ptr)
+        return NULL;
 
-	return alloc_buddy(buddy_allocator, size);
+    return alloc_buddy(buddy_allocator, size, order);
 }
 
-void * alloc_buddy(buddy_allocator_t *buddy_allocator, size_t size)
+void * alloc_buddy(buddy_allocator_t *buddy_allocator, size_t size, uint8_t * order)
 {
-	uint8_t curr = 0;
-	uint8_t order = 0;
-  	void * block = NULL;
-	void * buddy = NULL;
+    uint8_t curr = 0;
+    void * block = NULL;
+    void * buddy = NULL;
 
-  	while (ORDERTOSIZE(curr) < size + 1)
-	{ 
-    	curr++;
-	}
+    while (ORDERTOSIZE(curr) < size + 1)
+    { 
+        curr++;
+    }
 
-	if(curr < MIN_ORDER)
-	{
-		order = MIN_ORDER;
-		curr  = MIN_ORDER;
-	}
-	else
-	{
-		order = curr;
-	}
+    if(curr < MIN_ORDER)
+    {
+        *order = MIN_ORDER;
+        curr  = MIN_ORDER;
+    }
+    else
+    {
+        *order = curr;
+    }
 
-	while(curr <= buddy_allocator->maxOrder)
-	{
-		if (buddy_allocator->memPool[curr])
+    while(curr <= buddy_allocator->maxOrder)
+    {
+        if (buddy_allocator->memPool[curr])
             break;
-		curr++;
-	}
+        curr++;
+    }
 
-  	if (curr > buddy_allocator->maxOrder)
-		return NULL;
+    if (curr > buddy_allocator->maxOrder)
+    {
+        printf("Memory allocation fail!!! No free memory available.\n");
+        *order = 0;
+        return NULL;
+    }
 
-  	block = buddy_allocator->memPool[curr];
-  	buddy_allocator->memPool[curr] = *(void**)buddy_allocator->memPool[curr];
+    block = buddy_allocator->memPool[curr];
+    buddy_allocator->memPool[curr] = *(void**)buddy_allocator->memPool[curr];
 
-  	while (curr-- > order) 
-	{
-    	buddy = findBuddy(buddy_allocator, block, curr);
-    	buddy_allocator->memPool[curr] = buddy;
-  	}
+    while (curr-- > *order) 
+    {
+        buddy = findBuddy(buddy_allocator, block, curr);
+        buddy_allocator->memPool[curr] = buddy;
+    }
 
-	*((uint8_t*) (block - 1)) = order;
-  	return block;
+    return block;
 }
 
-void buddy_allocator_free(buddy_allocator_t *buddy_allocator, void *ptr)
+void buddy_allocator_free(buddy_allocator_t *buddy_allocator, void *ptr, uint8_t order)
 {
-	if(NULL == ptr)
-		return;
+    if(NULL == ptr)
+        return;
 
-	uint8_t curr = 0;
-  	void *  buddy= NULL;
-  	void ** memPtr = NULL;
+    uint8_t curr = 0;
+    void *  buddy= NULL;
+    void ** memPtr = NULL;
 
-  	curr = *((uint8_t*) (ptr - 1));
+    curr = order;
 
-  	for (;; curr++) 
-	{
-    	buddy = findBuddy(buddy_allocator, ptr, curr);
-    	memPtr = &(buddy_allocator->memPool[curr]);
+    for (;; curr++) 
+    {
+        buddy = findBuddy(buddy_allocator, ptr, curr);
+        memPtr = &(buddy_allocator->memPool[curr]);
 
-    	while ((*memPtr != NULL) && (*memPtr != buddy))
-      		memPtr = (void **) *memPtr;
+        while ((*memPtr != NULL) && (*memPtr != buddy))
+            memPtr = (void **) *memPtr;
 
-    	if (*memPtr != buddy) 
-		{
-      		*(void**) ptr = buddy_allocator->memPool[curr];
-      		buddy_allocator->memPool[curr] = ptr;
-      		return;
-    	}
-    	ptr = (ptr < buddy) ? ptr : buddy;
-    	*memPtr = *(void**) *memPtr;
-  	}
+        if (*memPtr != buddy) 
+        {
+            *(void**) ptr = buddy_allocator->memPool[curr];
+            buddy_allocator->memPool[curr] = ptr;
+            return;
+        }
+        ptr = (ptr < buddy) ? ptr : buddy;
+        *memPtr = *(void**) *memPtr;
+    }
 }
 
 void * findBuddy(buddy_allocator_t * buddyMem, void * block, uint8_t order)
 {
-	int offset = block - buddyMem->memStart;
-	return (void *)((offset ^ (1 << order)) + buddyMem->memStart); 
+    int offset = block - buddyMem->memStart;
+    return (void *)((offset ^ (1 << order)) + buddyMem->memStart); 
 }
 
